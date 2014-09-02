89b469a277282ba562b112bc02889fd53e5a911e
Kaushtuv <kaushtuvgurung@gmail.com>
2014-09-02 18:00:38 +1000

    updates

--- assets/js/micron21.js
+++ assets/js/micron21.js
@@ -20,47 +20,6 @@
  
     };
 	
-	// page modal lvl 2
-	$.fn.page_modal2 = function( action ) {
- 
-        if(action === 'show'){
-			// Open modal
-			this.modal('show');
-			$('.modal-backdrop').addClass('modal-backdrop-alt-2');	
-			$('.modal-backdrop-alt').removeClass('modal-backdrop-alt-2');
-			$('nav > ul > li > a').addClass('nav-pointer-backdrop-2');
-		}
- 
-        if ( action === "hide" ) {
-            // Close modal
-			this.modal('hide');
-			$('nav > ul > li > a').removeClass('nav-pointer-backdrop-2');
-        }
- 
-    };
-	
-	// action btns
-	$.fn.action_button = function(class_name){
-		class_name = typeof class_name !== 'undefined' ? class_name : 'btn-success';
-		var $this = $(this);
-		var target = $this.attr('data-target');
-		var len = 0;
-		var cur_classes = $(target).attr('class');
-		$this.on('keyup',function(){
-			len = $this.val().length;
-			if(len > 0){
-				// remove other btn classes
-				$(target).removeClass('btn-info').removeClass('btn-default').removeClass('btn-default-alt').removeClass('btn-warning').removeClass('btn-danger').removeClass('btn-success');
-				$(target).addClass(class_name);
-			}else{
-				$(target).removeAttr('class').addClass(cur_classes);
-			}
-		});
-		return this;
-		
-	};
-	
-	
 	// page modal lvl 1 action script
 	$(document).on('click','.pageModal',function(e){
 		var $this = $(this);
@@ -80,6 +39,25 @@
 		
 	});
 	
+	// page modal lvl 2
+	$.fn.page_modal2 = function( action ) {
+ 
+        if(action === 'show'){
+			// Open modal
+			this.modal('show');
+			$('.modal-backdrop').addClass('modal-backdrop-alt-2');	
+			$('.modal-backdrop-alt').removeClass('modal-backdrop-alt-2');
+			$('nav > ul > li > a').addClass('nav-pointer-backdrop-2');
+		}
+ 
+        if ( action === "hide" ) {
+            // Close modal
+			this.modal('hide');
+			$('nav > ul > li > a').removeClass('nav-pointer-backdrop-2');
+        }
+ 
+    };
+	
 	//page modal lvl 2 action script 
 	$(document).on('click','.pageModal2',function(e){
 		var $this = $(this);
@@ -98,6 +76,40 @@
 		$(target).page_modal2('hide');
 		
 	});
+	
+	// action btns
+	$.fn.action_button = function(target,class_name){
+		class_name = typeof class_name !== 'undefined' ? class_name : 'btn-success';
+		var $this = $(this);
+		var len = 0;
+		var cur_classes = $(target).attr('class');
+		console.log(cur_classes);
+		$this.on('keyup',function(){
+			len = $this.val().length;
+			if(len > 0){
+				// remove other btn classes
+				$(target).removeClass('btn-info').removeClass('btn-default').removeClass('btn-default-alt').removeClass('btn-warning').removeClass('btn-danger').removeClass('btn-success');
+				$(target).addClass(class_name);
+			}else{
+				$(target).removeAttr('class').addClass(cur_classes);
+			}
+		});
+		return this;
+		
+	};
+	
+	// action btn action script
+	$(document).on('focus','.actionBtn',function(){
+		var $this = $(this);
+		var target = $this.attr('data-target');
+		var append_class = $this.attr('data-class');
+		$('.actionBtn').action_button(target,append_class);
+	});
+	
+	
+	
+	
+	
 
  
 }( jQuery ));
